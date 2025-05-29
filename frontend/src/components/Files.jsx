d13e5023ff1c8f0247f30dd640c77ca21fd44259
Nightingale <93238522+CITATS928@users.noreply.github.com>
2025-05-29 14:31:57 -0400

    record test

--- frontend/src/components/Files.jsx
+++ frontend/src/components/Files.jsx
@@ -26,6 +26,47 @@ export default function Files() {
 
 
   return (
-    
+    <div style={{ backgroundColor: "#f8f9fa", minHeight: "100vh", paddingTop: "60px", width: "1000px" }}>
+    <div className="container mt-5">
+      <ToastContainer />
+      <h2 className="mb-4">My Files Records</h2>
+
+      {items.length === 0 ? (
+       <p>You haven't uploaded any file.</p>
+      ) : (
+        <table className="table table-bordered table-hover">
+          <thead className="table-dark">
+            <tr>
+              <th>#</th>
+              <th>Entity</th>
+              <th>Account Code</th>
+              <th>Description</th>
+              <th>YTD Actual</th>
+              <th>Annual Budget</th>
+              <th>Category</th>
+              <th>Item Type</th>
+              <th>Expense Nature</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item, index) => (
+              <tr key={item.id}>
+                <td>{index + 1}</td>
+                <td>{item.entity_name}</td>
+                <td>{item.account_code}</td>
+                <td>{item.description}</td>
+                <td>{item.ytd_actual}</td>
+                <td>{item.annual_budget}</td>
+                <td>{item.category}</td>
+                <td>{item.item_type}</td>
+                <td>{item.expense_nature}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+
+    </div>
+    </div>
   );
 }
