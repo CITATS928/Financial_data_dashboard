e89336c53e745871e792947da7295dc639db5c5c
Nightingale <93238522+CITATS928@users.noreply.github.com>
2025-06-13 12:25:45 -0400

    change setting for develop

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$sf))by-hd4)b#52)xocr@%cn2u4z^-&ix$8gae&b#1zv8sly5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
