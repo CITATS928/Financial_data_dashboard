bc053eba14dacc6fe7d973427b964beee1e572fe
Nightingale <93238522+CITATS928@users.noreply.github.com>
2025-07-13 07:37:51 -0400

    admin test

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import UploadedFile
+from .models import UploadedFile, FinancialLineItem
 admin.site.register(UploadedFile)
+admin.site.register(FinancialLineItem)
