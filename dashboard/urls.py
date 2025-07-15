d28279f7afc1900b6b5c7fb55a71b637e407d7cf
Lavanya Palanikumar <lavanyasaii93@gmail.com>
2025-07-15 13:24:18 -0400

    Fixed bug in quarterly report

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     # path('api/total-actual-by-entity/', total_actual_by_entity),
     path('api/aggregate_report/', aggregate_report),
     path('api/entity-yearly-actual/<str:entity_name>/', views.entity_yearly_actual),
+    path('api/entity-quarterly-actual/<str:entity_name>/', views.entity_quarterly_actual),
     path('api/entities/', views.get_entity_names, name='get_entity_names'),
     path('api/entities/<str:entity_name>/details/',  views.get_entity_detail),
     path('api/all-items/', views.get_all_items),
