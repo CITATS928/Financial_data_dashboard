b990abf638bfdbba209fd7771a04f0bf1680f0cd
Nightingale <93238522+CITATS928@users.noreply.github.com>
2025-07-17 08:15:47 -0400

    remove urls that won't be use

--- dashboard/api_urls.py
+++ dashboard/api_urls.py
@@ -20,7 +20,8 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('upload/', UploadCSVView.as_view(), name='upload-csv'),
+    # path('upload/', UploadCSVView.as_view(), name='upload-csv'),
+    path('upload/', UploadDynamicCSVView.as_view(), name='upload'),
     path('table/', FinancialDataView.as_view(), name='financial-data'),
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('signup/', signup_api_view, name='signup'),
@@ -28,12 +29,11 @@ urlpatterns = [
     path('dashboard/session-login/', SessionLoginView.as_view(), name='session_login'),
     path('logout/', LogoutAPIView, name='api_logout'),
     path('financial-line-items/', FinancialLineItemsListView.as_view(), name='financial-line-items'),
-    path('upload-financial-line-items/', UploadFinancialLineItemsView.as_view(), name='upload-financial-line-items'),
+    # path('upload-financial-line-items/', UploadFinancialLineItemsView.as_view(), name='upload-financial-line-items'),
     path("current-user/", CurrentUserView.as_view(), name="current_user"),
     path("update-profile/", UpdateProfileView.as_view(), name="update_profile"),
-    # path("my-files/", MyUploadedItemsView.as_view(), name="my_uploaded_items"),
     path("my-uploaded-files/", MyUploadedFilesView.as_view(), name="my_uploaded_files"),
-    path("upload-dynamic-csv/", UploadDynamicCSVView.as_view(), name="upload_dynamic_csv"),
+    # path("upload-dynamic-csv/", UploadDynamicCSVView.as_view(), name="upload_dynamic_csv"),
     path('aggregate-report/', views.aggregate_report, name='aggregate-report'),
     path('entities/', views.get_entity_names, name='get_entity_names'),
     path('entity-yearly-actual/<str:entity_name>/', views.entity_yearly_actual, name='entity_yearly_actual'),
